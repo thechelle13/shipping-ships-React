85d9b9c340ab028328a347a3dc667908c50d52e7
Michelle Totherow <tym4me13@gmail.com>
2023-10-30 22:55:33 -0500

    update services, components and forms

--- src/components/forms/EditHauler.jsx
+++ src/components/forms/EditHauler.jsx
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom"
 export const EditHauler = () => {
     const [docks, setDocks] = useState([])
     const [haulers, setHaulers] = useState([])
-    const navigate = useNavigate()
+    const Navigate = useNavigate()
 
 
     useEffect(()=> {
@@ -13,15 +13,46 @@ export const EditHauler = () => {
     }, [])
 
 
-    // getAllDocks().then((dockObject) => {
-    //     setDocks(dockObject)
-    // },[])
+    useEffect(()=> {
+        getAllHaulers().then((haulerArray) => {
+            setHaulers(haulerArray)
+        })
+    }, [])
 
-    handleSave =() => {
+    // handleSave =() => {
         
-    }
+    // }
 
     return <>
    
     </>
 }
+
+// const handleInputChange = (evt) => {
+//     const copy = { ...owner };
+//     copy[evt.target.id] = evt.target.value;
+//     setOwner(copy);
+//     };
+
+//     const handleSave = (event) => {
+//     event.preventDefault();
+
+//     const updatedUser = {
+//         id: currentUser.id,
+//         fullName: owner.fullName,
+//         isPetOwner: owner.isPetOwner,
+//         city: owner.city,
+//         email: owner.email,
+//     };
+
+//     userEdited(updatedUser)
+//         .then((res) => {
+//         //console.log("API Response:", res);
+//         setCurrentUser(res); 
+//         Navigate(`/Owners`);
+//         })
+//         .catch((error) => {
+//         //console.error("Error updating user data:", error);
+            
+//         });
+//     };
